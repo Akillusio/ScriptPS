2539d807f80aba41c17e877f12cc67cd763833b6
Akillusio <45083205+Akillusio@users.noreply.github.com>
2019-05-13 18:02:29 +0200

    ajout du script creation utilisateur ad
    version : 1.0
    ajout des commandes pour ajouter le module ad
    écriture du script
    quelques erreurs , script incomplet

---
+++ testscripta.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+Import-Module ntfssecurity
+$leprenom=Read-Host "entrez le prénom de l'utilisateur"
+$lesurnom=Read-Host "entrez le surnom de l'utilisateur"
+$defaultmdp = "Chantellie@78"
+$lenom= "$leprenom $lesurnom"
+$leou=Read-Host "entrez son unité d'organisation"
+$path="OU=$leou,OU=Acme group,DC=acme,DC=fr"
+$mdpsecure= ConvertTo-SecureString $defaultmdp -AsPlainText -Force
+$nomducompte= "$leprenom.$lesurnom"
+New-Item -Name $lenom -ItemType Directory -Path E:\Shares\aduser
+$pathhd="E:\Shares\aduser\$lenom"
+
+New-ADUser -GivenName $leprenom -Surname $lesurnom -SamAccountName $nomducompte -Name $lenom -Path $path -AccountPassword $mdpsecure -UserPrincipalName "$nomducompte@acme.fr" -HomeDirectory $pathhd -Enabled $true
+echo "terminé"
