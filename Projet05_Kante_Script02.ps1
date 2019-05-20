1e8c912a320ca370e25c9df88d64c7f4ee150f07
Akillusio <45083205+Akillusio@users.noreply.github.com>
2019-05-20 16:34:29 +0200

    version 1.0
    -commit initial,
    -ajout des fonctionnalités principales

---
+++ Projet05_Kante_Script02.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+Set-ExecutionPolicy Unrestricted
+
+$ngroup= Read-Host "Entrez le nom du groupe"
+#récupère le nom du groupe , liste ses membres et écrit le résultat dans le répertoire sur un fichier .txt.
+Get-ADGroupMember -Identity $ngroup | Out-File C:\Users\Administrateur\Desktop\MonPremierRepo\Script_ps\Script02\Projet05_Kante_AD02.txt
+echo "terminé"
