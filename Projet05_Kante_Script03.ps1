6dec2d7d78bd4db33f551efd75f1b1679809b094
Akillusio <45083205+Akillusio@users.noreply.github.com>
2019-05-21 17:20:31 +0200

    version 1.0
    Commit initial
    ajout commentaire

---
+++ Projet05_Kante_Script03.ps1
@@ -0,0 +1,5 @@
+﻿Import-module ActiveDirectory
+ 
+$nomuser = Read-Host "Entrez le nom de l'utilisateur AD:"
+#recupere la liste des groupes dont est membre un utilisateur
+Get-ADPrincipalGroupMembership -Identity $nomuser | Out-File C:\Users\Administrateur\Desktop\MonPremierRepo\Script_ps\Script03\Projet05_Kante_AD03.txt
