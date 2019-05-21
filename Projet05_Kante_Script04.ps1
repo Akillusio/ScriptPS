29265e95e98c2f4f6ffaadcf91cbe7560536cc6a
Akillusio <45083205+Akillusio@users.noreply.github.com>
2019-05-22 01:52:51 +0200

    version 1.0
    Initialisation
    ajout de la fonction pour creer un dossier de sauvegarde de l'utilisateur si celui ci n'existe pas

---
+++ Projet05_Kante_Script04.ps1
@@ -0,0 +1,13 @@
+﻿
+$nomuser = [Environment]::UserName
+$pathgen = "\\Srvdcpar001\sav"
+$pathdos = "\\Srvdcpar001\sav\$nomuser"
+
+#Je vérifie si un dossier à son nom existe deja, sinon , je le cree
+
+If (-not (Test-Path $pathdos))
+{
+    New-Item -ItemType Directory -Name $nomuser -Path $pathgen
+}
+
+Copy-Item
