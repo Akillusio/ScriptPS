30aecfba3df02c1ef8833c277040b1459fa5920e
Akillusio <45083205+Akillusio@users.noreply.github.com>
2019-05-23 03:33:10 +0200

    version 2.0
    Suppression ligne executionpolicy car politique d'execution déjà définis dans les gpo

--- Projet05_Kante_Script04.ps1
+++ Projet05_Kante_Script04.ps1
@@ -2,7 +2,7 @@
 $nomuser = [Environment]::UserName
 $pathgen = "\\Srvdcpar001\sav"
 $pathdos = "\\Srvdcpar001\sav\$nomuser"
-
+$pathloc = "C:\Users\$nomuser"
 #Je vérifie si un dossier à son nom existe deja, sinon , je le cree
 
 If (-not (Test-Path $pathdos))
@@ -10,4 +10,5 @@ If (-not (Test-Path $pathdos))
     New-Item -ItemType Directory -Name $nomuser -Path $pathgen
 }
 
-Copy-Item
+xcopy $pathloc $pathdos /D /E /C /R /H /I /K /Y
+echo "terminé"
